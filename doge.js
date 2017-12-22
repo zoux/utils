932507cc18fe707271a1210fe3be161bc3d9d4cd
zx <zoux@ecworking.com>
2017-12-22 14:41:33 +0800

    字符串模块 - replace(str, find, rep) 关键词替换

--- doge.js
+++ doge.js
@@ -7,9 +7,9 @@
  * 内容结构：
  *
  * --- 字符串模块
- * ------ trim(str, type)       去除空格
- * ------ changeCase(str, type) 大小写转换
- *
+ * ------ trim(str, type)         去除空格
+ * ------ changeCase(str, type)   大小写转换
+ * ------ replace(str, find, rep) 关键词替换
  */
 
 
@@ -17,8 +17,10 @@ const doge = {
   /**
    * trim(str, type)
    *
-   * @str   {string} 待处理的字符串
-   * @type  {number} 1-前后空格 2-所有空格 3-前空格 4-后空格
+   * @str     {string} 待处理的字符串
+   * @type    {number} 1-前后空格 2-所有空格 3-前空格 4-后空格
+   *
+   * @return  {string} 处理后的字符串
    */
   trim(str, type = 1) {
     switch (type) {
@@ -38,8 +40,10 @@ const doge = {
   /**
    * changeCase(str, type)
    *
-   * @str   {string} 待处理的字符串
-   * @type  {number} 1-全部大写 2-全部小写 3-首字母大写 4-大小写转换
+   * @str     {string} 待处理的字符串
+   * @type    {number} 1-全部大写 2-全部小写 3-首字母大写 4-大小写转换
+   *
+   * @return  {string} 处理后的字符串
    */
   changeCase(str, type = 1) {
     switch (type) {
@@ -67,9 +71,23 @@ const doge = {
       default:
         return str;
     }
-  }
+  },
+
+  /**
+   * replace(str, find, rep)
+   *
+   * @str     {string} 待处理的字符串
+   * @find    {string} 被替换的关键词
+   * @rep     {any}    替换为什么
+   *
+   * @return  {string} 处理后的字符串
+   */
+  replace(str, find, rep) {
+    const RegExpResult = new RegExp(find, 'g');
+    return str.replace(RegExpResult, rep);
+  },
 };
 
 console.log(
-  doge.changeCase('哈哈哈aaa', 3)
+  doge.replace('哈哈哈aaa', 'a', {})
 );
