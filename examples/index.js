807def2bfb8a2f478ba81d19721c648998173955
zouxin <zouxin@iboxpay.com>
2019-01-12 13:40:11 +0800

    剔除 recursive

--- examples/index.js
+++ examples/index.js
@@ -2,6 +2,5 @@ import utils from '../src/index'
 
 import './test/loadJsSync'
 import './test/preloadImg'
-import './test/recursive'
 
 console.log(utils)
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import loadJsSync from './module/loadJsSync'
 import preloadImg from './module/preloadImg'
-import recursive from './module/recursive'
 
 const utils = {
   loadJsSync,
   preloadImg,
-  recursive
 }
 
 if (window && !window.utils) {
   window.utils = utils
 }
 
-export { loadJsSync, preloadImg, recursive }
+export { loadJsSync, preloadImg }
 
 export default utils
